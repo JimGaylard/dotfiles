46497c3efec67dec9fbd92ca4e4dde22d1960d30
Jim Gaylard <jgaylard75@gmail.com>
2012-10-02 22:42:47 +1000

    More changes for spf13 vim

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -9,7 +9,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+files="vimrc.local vimrc.bundles.local"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -31,28 +31,3 @@ for file in $files; do
     ln -s $dir/$file ~/.$file
 done
 
-function install_zsh {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    echo "zsh is installed"
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
-    fi
-else
-    echo "zsh not installed"
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        sudo apt-get install zsh
-        install_zsh
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
-    fi
-fi
-}
-
-install_zsh
