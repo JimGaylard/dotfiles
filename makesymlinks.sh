6f5f1446551daeb85a2afda2499d7100c07a876b
Jim Gaylard <jgaylard75@gmail.com>
2013-06-27 21:52:59 +1000

    blah

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 ############################
 # .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
@@ -9,7 +9,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc.local vimrc.bundles.local zshrc tmux.conf"    # list of files/folders to symlink in homedir
+files="vimrc.local vimrc.bundles.local tmux.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
