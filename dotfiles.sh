427a1c346e7b56567917aa97c347f1dfa0ea9361
Jim Gaylard <jgaylard75@gmail.com>
2018-02-22 19:30:02 +1100

    fixes script

--- dotfiles.sh
+++ dotfiles.sh
@@ -6,6 +6,9 @@ set -eu pipefail
 cd $HOME/dotfiles
 git diff HEAD --exit-code
 
+. $HOME/.keychain/$HOSTNAME-sh
+
+git fetch
 if [[ $(git log HEAD..origin/master --oneline) ]]; then
   echo 'remote changes to dotfiles'
   exit 1
